aecaf5aa8c3480a3901a1b5ec16d4b6db3595823
puntodamar <punto@privyid.tech>
2025-07-30 12:41:49 +0700

    add:  tailwind

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel</title>
     @routes
+    <style>
+        @import "tailwindcss";
+    </style>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
-<body>
+<body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
     @inertia
 </body>
 </html>
